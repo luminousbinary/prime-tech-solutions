e272ad4b85cba73addc7317ebc1c969119c62eac
luminousbinary <boolwrld@outlook.com>
2024-07-17 18:25:45 +0100

    cds

--- src/_components/CourseDescription.tsx
+++ src/_components/CourseDescription.tsx
@@ -2,8 +2,8 @@ import { Flex } from '@mantine/core'
 
 const CourseDescription = () => {
   return (
-    <div className="w-full lg:pl-16 space-y-10 ">
-    <h1 className="text-lg font-medium text-black font-serif">
+    <div className="w-full md:pl-16 space-y-10 ">
+    <h1 className="lg:text-lg md:text-base font-medium text-black font-serif">
       Our Game Development course covers the core principles of designing
       and creating interactive games. Youll learn about game mechanics,
       storytelling, level design, and programming using popular game
@@ -11,8 +11,8 @@ const CourseDescription = () => {
       experiences from concept to completion.
     </h1>
     <Flex className="gap-6 items-center">
-        <button className="bg-gradient-to-r from-[#100866] to-[#2011CC] rounded-[2rem] bg-[#100866] text-white py-2 px-6 shadow-md font-semibold text-lg">Enroll now</button>
-        <span className="font-bold text-black text-2xl">$150.00</span>
+        <button className="bg-gradient-to-r from-[#100866] to-[#2011CC] rounded-[2rem] bg-[#100866] text-white py-2 px-6 shadow-md font-semibold md:text-base lg:text-lg">Enroll now</button>
+        <span className="font-bold text-black md:text-lg lg:text-2xl">$150.00</span>
     </Flex>
   </div>
   )
